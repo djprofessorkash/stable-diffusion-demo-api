9b007bb779a9105f49b013d9e7d8bc7a862d7df0
Aakash Sudhakar <aakash.sudhakar@gmail.com>
2023-05-18 08:12:25 -0400

    [ADD] Created main index file for general React instantiation and config

---
+++ frontend/src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "bulma/css/bulma.min.css";
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// root.render(<App />);
+
+ReactDOM.render(<App />, document.getElementById("root"));
